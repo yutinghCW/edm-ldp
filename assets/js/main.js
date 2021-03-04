f0e157bcc979f2a362b0a2800a2bbc57e3c7d4a6
yutingh <yutingh@cw.com.tw>
2021-03-04 10:14:06 +0800

    簡化成共用寫法

--- assets/js/main.js
+++ assets/js/main.js
@@ -35,6 +35,49 @@ $(function () {
 		$('.black').removeClass('opened');
 		$('body').removeClass('message__open');
 	});
+	$(".form button").click(function() {
+		var mail = $('#email').val(),
+			paper = $('#email').attr('data-paper');
+		// console.log(paper);
+		function validateEmail(mail) {
+			const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+			return re.test(mail);
+		}
+		if ( validateEmail(mail) ) {
+			$('#email').parent().removeClass('form__group--error').addClass('form__group--correct');
+		} else if ( mail == '' ) {
+			$('.form__group__help').text('請輸入電子郵件');
+			$('#email').parent().removeClass('form__group--correct').addClass('form__group--error');
+			return false;
+		} else {
+			$('.form__group__help').text('請輸入正確格式');
+			$('#email').parent().removeClass('form__group--correct').addClass('form__group--error');
+			return false;
+		}
+		$.post("https://www.cw.com.tw/api/newsletter/outer/subscribe", {
+			email: mail,
+			code: paper,
+			function(d) {
+				$('.loader').fadeIn();
+				setTimeout(() => {
+					$('.loader').hide();
+					$('#error').show();
+					$('.black').addClass('opened');
+				}, 5000);
+			}
+		}, function(data, status) {
+			// alert("Data: " + data + "\nStatus: " + status);
+			if (status == 'success') {
+				$('.loader, #error').hide();
+				$('#success').show();
+				$('.black').addClass('opened');
+			} else {
+				$('.loader').hide();
+				$('#error').show();
+				$('.black').addClass('opened');
+			}
+		});
+	});
 	$(window).resize(function () {});
 	$(window).scroll(function () {
 		var scroll = $(this).scrollTop(),
